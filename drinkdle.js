72e094a14f620de2e06b824b50ec6573ba784526
v <261508724+vnoblitt@users.noreply.github.com>
2026-07-22 05:38:31 -0500

    Made base logic of app. Could not get CocktailDB to work so I added a pseudodatabase of my own. The CSS kept squishing thinbs together so String.fromCharCode(160) is just for a no break space.

---
+++ drinkdle.js
@@ -0,0 +1,62 @@
+const input = document.getElementById('input');
+const output = document.getElementById('output');
+
+input.addEventListener('keydown', function(event) {
+    if (event.key == 'Enter') {
+        checkGuess(input.value);
+        input.value = '';
+    };
+});
+
+function checkGuess(input) {
+    let guessEntry;
+
+    if (!drinks.some((drink) => drink.name.toLowerCase() === input.toLowerCase())) {
+        console.log('drink not in database');
+        return;
+    } else {
+        guessEntry = drinks.find((drink) => drink.name.toLowerCase() === input.toLowerCase());
+    }
+
+    const guessName = document.createElement('div');
+    const guessSpirit = document.createElement('div');
+    guessName.textContent = `${guessEntry.name}:` + String.fromCharCode(160);
+    guessSpirit.textContent = guessEntry.spirit;
+
+    if (guessEntry === correctGuess()) {
+        guessName.classList.add('right');
+    } else {
+        guessName.classList.add('wrong');
+    }
+
+    if (guessEntry.spirit === correctGuess().spirit) {
+        guessSpirit.classList.add('right');
+    } else {
+        guessSpirit.classList.add('wrong');
+    }
+
+    output.appendChild(guessName);
+    output.appendChild(guessSpirit);
+}
+
+function correctGuess() {
+    return drinks[1];
+}
+
+const drinks = [
+    {
+        name: 'Margarita',
+        ingredients: ['triple-sec', 'lime juice', 'salt'],
+        spirit: 'tequila',
+    },
+    {   
+        name: 'Paloma',
+        ingredients: ['grapefruit soda', 'lime juice', 'salt'],
+        spirit: 'tequila', 
+    },
+    {
+        name: 'Old Fashioned',
+        ingredients: ['bitters', 'sugar', 'orange'],
+        spirit: 'whiskey',
+    }    
+];
