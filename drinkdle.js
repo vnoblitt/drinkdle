bd440a6b1ade48a823c7b963f7be2a278935d338
v <261508724+vnoblitt@users.noreply.github.com>
2026-07-23 00:47:00 -0500

    Finished what I'm content to call version 0.1. Everything works and the game is clear enough if you've played daily games before. There's still a lack of recognized drinks, but I'll figure out how to take track what's been submitted and add to the database later. Or figure out how to use an API.

--- drinkdle.js
+++ drinkdle.js
@@ -1,5 +1,26 @@
 const input = document.getElementById('input');
 const output = document.getElementById('output');
+const resetButton = document.getElementById('reset');
+const submitButton = document.getElementById('submit');
+const hard = document.getElementById('hard');
+
+let hardMode = false;
+hard.addEventListener('change', (event) => {
+    if (event.target.checked) {
+        hardMode = true;
+    } else {
+        hardMode = false;
+    }
+});
+
+resetButton.addEventListener('click', () =>  {
+    output.innerHTML = '';
+});
+
+submitButton.addEventListener('click', () => {
+    checkGuess(input.value);
+    input.value = '';
+})
 
 input.addEventListener('keydown', function(event) {
     if (event.key == 'Enter') {
@@ -11,17 +32,45 @@ input.addEventListener('keydown', function(event) {
 function checkGuess(input) {
     let guessEntry;
 
+    const guessRow1 = document.createElement('div');
+    const guessRow2 = document.createElement('div');
+    const guessName = document.createElement('div');
+    const guessSpirit = document.createElement('div');
+    const guessIngredientsCorrect = document.createElement('div');
+    const guessIngredientsWrong = document.createElement('div');
+
     if (!drinks.some((drink) => drink.name.toLowerCase() === input.toLowerCase())) {
-        console.log('drink not in database');
+        guessRow1.textContent = `${input} not in database`;
+        output.appendChild(guessRow1);
         return;
     } else {
         guessEntry = drinks.find((drink) => drink.name.toLowerCase() === input.toLowerCase());
     }
 
-    const guessName = document.createElement('div');
-    const guessSpirit = document.createElement('div');
-    guessName.textContent = `${guessEntry.name}:` + String.fromCharCode(160);
-    guessSpirit.textContent = guessEntry.spirit;
+    
+    guessRow1.classList.add('guess-row1');
+    guessRow2.classList.add('guess-row2');
+    guessName.textContent = `${guessEntry.name}`;
+    guessSpirit.textContent = `Spirit: ${guessEntry.spirit}`;
+    if (!hardMode) {
+        let matchedIngredients = [];
+        let unmatchedIngredients = [];
+
+        for(let ingredient of guessEntry.ingredients) {
+            console.log(ingredient);
+            if (correctGuess().ingredients.includes(ingredient)) {
+                matchedIngredients.push(ingredient);
+            } else {
+                unmatchedIngredients.push(ingredient);
+            }
+        }
+        let cleanMatched = matchedIngredients.join(', ');
+        let cleanUnmatched = unmatchedIngredients.join(', ');
+        guessIngredientsCorrect.textContent = cleanMatched
+        guessIngredientsWrong.textContent = cleanUnmatched;
+        guessIngredientsCorrect.classList.add('right');
+        guessIngredientsWrong.classList.add('wrong');
+    } 
 
     if (guessEntry === correctGuess()) {
         guessName.classList.add('right');
@@ -35,8 +84,14 @@ function checkGuess(input) {
         guessSpirit.classList.add('wrong');
     }
 
-    output.appendChild(guessName);
-    output.appendChild(guessSpirit);
+    guessRow1.appendChild(guessName);
+    guessRow1.appendChild(guessSpirit);
+    guessRow2.appendChild(guessIngredientsWrong);   // These two are flipped for aesthetic reasons
+    guessRow2.appendChild(guessIngredientsCorrect); // even though it looks sloppy in code
+    output.appendChild(guessRow1);
+    if (!hardMode) {
+        output.appendChild(guessRow2);
+    }
 }
 
 function correctGuess() {
@@ -47,21 +102,21 @@ const drinks = [
     {
         name: 'Margarita',
         ingredients: ['triple-sec', 'lime juice', 'salt'],
-        spirit: 'tequila',
+        spirit: 'Tequila',
     },
     {   
         name: 'Paloma',
         ingredients: ['grapefruit soda', 'lime juice', 'salt'],
-        spirit: 'tequila', 
+        spirit: 'Tequila', 
     },
     {
         name: 'Old Fashioned',
         ingredients: ['bitters', 'sugar', 'orange'],
-        spirit: 'whiskey',
+        spirit: 'Whiskey',
     },
     {
         name: 'Moscow Mule',
         ingredients: ['ginger ale', 'lime juice'],
-        spirit: 'vodka',
+        spirit: 'Vodka',
     }    
 ];
