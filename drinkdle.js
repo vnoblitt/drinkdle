90fef9db34c865eed2141a999c2d4843a6d91ed7
v <261508724+vnoblitt@users.noreply.github.com>
2026-07-23 05:27:30 -0500

    Completed the database and added win-loss functionality.

--- drinkdle.js
+++ drinkdle.js
@@ -1,10 +1,17 @@
+import { drinks } from './drinks.js';
+
 const input = document.getElementById('input');
 const output = document.getElementById('output');
-const resetButton = document.getElementById('reset');
+const revealButton = document.getElementById('reveal');
 const submitButton = document.getElementById('submit');
 const hard = document.getElementById('hard');
+const attempts = document.getElementById('attempts');
 
 let hardMode = false;
+let won = false;
+let remainingAttempts = 4;
+updateAttempts()
+
 hard.addEventListener('change', (event) => {
     if (event.target.checked) {
         hardMode = true;
@@ -13,25 +20,82 @@ hard.addEventListener('change', (event) => {
     }
 });
 
-resetButton.addEventListener('click', () =>  {
-    output.innerHTML = '';
+revealButton.addEventListener('click', () =>  {
+    if (!won) {
+        remainingAttempts = 0;
+        updateAttempts();
+        revealAnswer();
+    }
 });
 
 submitButton.addEventListener('click', () => {
-    checkGuess(input.value);
-    input.value = '';
+    if (remainingAttempts) {
+        remainingAttempts--;
+        updateAttempts();
+        checkGuess(input.value);
+        input.value = '';
+    }
 })
 
 input.addEventListener('keydown', function(event) {
     if (event.key == 'Enter') {
-        checkGuess(input.value);
-        input.value = '';
+        if (remainingAttempts) {
+            remainingAttempts--;
+            updateAttempts();
+            checkGuess(input.value);
+            input.value = '';
+        }
     };
 });
+
+function updateAttempts() {
+    attempts.textContent = `Remaining Guesses: ${remainingAttempts}`;
+}
+function gameWon() {
+    const winnerText = document.createElement('div');
+    winnerText.textContent = 'You Win!';
+    output.appendChild(winnerText);
+    won = true;
+}
+
+function gameLost() {
+    revealAnswer();
+    const loserText = document.createElement('div');
+    loserText.textContent = 'You Lose.';
+    output.appendChild(loserText);
+}
+
+function revealAnswer() {
+    const answer = getDrink();
+
+    const answerRow1 = document.createElement('div');
+    const answerRow2 = document.createElement('div');
+    const answerName = document.createElement('div');
+    const answerSpirit = document.createElement('div');
+    const answerIngredients = document.createElement('div');
+
+    answerRow1.classList.add('row1');
+    answerRow2.classList.add('row2');
+
+    answerName.textContent = `${answer.name}`;
+    answerSpirit.textContent = `Spirit: ${answer.spirit}`;
+    let ingredients = [];
+    for (let ingredient of answer.ingredients) {
+        ingredients.push(ingredient);
+    }
+    answerIngredients.textContent = ingredients.join(', '); 
+
+    answerRow1.appendChild(answerName);
+    answerRow1.appendChild(answerSpirit);
+    answerRow2.appendChild(answerIngredients);
+    output.appendChild(answerRow1);
+    output.appendChild(answerRow2); 
+}
 
 function checkGuess(input) {
     let guessEntry;
     let cleanInput = input.trim();
+    
     const guessRow1 = document.createElement('div');
     const guessRow2 = document.createElement('div');
     const guessName = document.createElement('div');
@@ -39,26 +103,39 @@ function checkGuess(input) {
     const guessIngredientsCorrect = document.createElement('div');
     const guessIngredientsWrong = document.createElement('div');
 
-    if (!drinks.some((drink) => drink.name.toLowerCase() === cleanInput.toLowerCase())) {
-        guessRow1.textContent = `${cleanInput} not in database`;
-        output.appendChild(guessRow1);
-        return;
-    } else {
-        guessEntry = drinks.find((drink) => drink.name.toLowerCase() === cleanInput.toLowerCase());
+    if (cleanInput === '') {
+        /* The database uses blank strings for alt drink
+            names. So we check if the string is blank before
+            working on it. This gives you back an attempt. */
+        remainingAttempts += 1;
+        return updateAttempts();
     }
 
+    if (!drinks.some((drink) => drink.name.toLowerCase() === cleanInput.toLowerCase() 
+        || drink.altName.toLowerCase() === cleanInput.toLowerCase())) {
+
+        guessRow1.textContent = `${cleanInput} is not in the database`;
+        output.appendChild(guessRow1);
+        remainingAttempts++;
+        return updateAttempts();
+    } else {
+        if (drinks.some((drink) => drink.altName.toLowerCase() === cleanInput.toLowerCase())) {
+            guessEntry = drinks.find((drink) => drink.altName.toLowerCase() === cleanInput.toLowerCase());
+        } else {
+            guessEntry = drinks.find((drink) => drink.name.toLowerCase() === cleanInput.toLowerCase());
+        }
+    }
     
-    guessRow1.classList.add('guess-row1');
-    guessRow2.classList.add('guess-row2');
-    guessName.textContent = `${guessEntry.name}`;
+    guessRow1.classList.add('row1');
+    guessRow2.classList.add('row2');
+    guessName.textContent = `Drink: ${guessEntry.name}`;
     guessSpirit.textContent = `Spirit: ${guessEntry.spirit}`;
     if (!hardMode) {
         let matchedIngredients = [];
         let unmatchedIngredients = [];
 
         for(let ingredient of guessEntry.ingredients) {
-            console.log(ingredient);
-            if (correctGuess().ingredients.includes(ingredient)) {
+            if (getDrink().ingredients.includes(ingredient)) {
                 matchedIngredients.push(ingredient);
             } else {
                 unmatchedIngredients.push(ingredient);
@@ -72,13 +149,13 @@ function checkGuess(input) {
         guessIngredientsWrong.classList.add('wrong');
     } 
 
-    if (guessEntry === correctGuess()) {
+    if (guessEntry === getDrink()) {
         guessName.classList.add('right');
     } else {
         guessName.classList.add('wrong');
     }
 
-    if (guessEntry.spirit === correctGuess().spirit) {
+    if (guessEntry.spirit === getDrink().spirit) {
         guessSpirit.classList.add('right');
     } else {
         guessSpirit.classList.add('wrong');
@@ -91,32 +168,28 @@ function checkGuess(input) {
     output.appendChild(guessRow1);
     if (!hardMode) {
         output.appendChild(guessRow2);
+    
+    }
+    if (guessEntry.name === getDrink().name) {
+        return gameWon();
+    } else if (!remainingAttempts) {
+        return gameLost();
     }
 }
 
-function correctGuess() {
-    return drinks[1];
+function getDrink() {
+    const now = new Date();
+    return drinks[now.getDate() - 1];
 }
 
-const drinks = [
-    {
-        name: 'Margarita',
-        ingredients: ['triple-sec', 'lime juice', 'salt'],
-        spirit: 'Tequila',
-    },
-    {   
-        name: 'Paloma',
-        ingredients: ['grapefruit soda', 'lime juice', 'salt'],
-        spirit: 'Tequila', 
-    },
-    {
-        name: 'Old Fashioned',
-        ingredients: ['bitters', 'sugar', 'orange'],
-        spirit: 'Whiskey',
-    },
-    {
-        name: 'Moscow Mule',
-        ingredients: ['ginger ale', 'lime juice'],
-        spirit: 'Vodka',
-    }    
+function getArray() {
+    return drinks.length;
+}
+
+console.log(getArray());
+
+const loserDrinks = [
+    'Vodka Martini', 'Pisco Sour', 'Screwdriver', 'Vesper',
+    'Adios', 'AMF', 'Adios Motherfucker', 'Long Island Iced Tea',
+    'Long Island', 'LIT',
 ];
---
+++ drinks.js
@@ -0,0 +1,198 @@
+// drinks.js
+
+export const drinks = [
+    {
+        name: 'Aviation',
+        ingredients: ['lemon juice', 'maraschino liqueur'],
+        spirit: 'Gin',
+        altName: '',
+    },
+    {
+        name: 'Bahama Mama',
+        ingredients: ['pineapple juice', 'orange juice', 'grenadine', 'sour mix'],
+        spirit: 'Rum',
+        altName: '',
+    },
+    {
+        name: "Bee's Knees",
+        ingredients: ['lemon juice', 'honey'],
+        spirit: 'Gin',
+        altName: 'Bees Knees',
+    },
+    {
+        name: 'Boulevardier',
+        ingredients: ['campari', 'sweet vermouth', 'orange peel'],
+        spirit: 'Whiskey',
+        altName: '',
+    },
+    {
+        name: 'Bramble',
+        ingredients: ['lemon juice', 'sugar', 'blackberry liqueur'],
+        spirit: 'Gin',
+        altName: '',
+    },
+    {
+        name: 'Clover Club',
+        ingredients: ['grenadine', 'lemon juice', 'egg white'],
+        spirit: 'Gin',
+        altName: '',
+    },
+    {
+        name: 'Cosmopolitan',
+        ingredients: ['cranberry juice', 'lime juice', 'triple sec'],
+        spirit: 'Vodka',
+        altName: 'Cosmo',
+    },
+    {
+        name: 'Dark n Stormy',
+        ingredients: ['ginger beer', 'lime juice'], 
+        spirit: 'Rum',
+        altName: 'Dark and Stormy',
+    },
+    {
+        name: 'Bloody Mary',
+        ingredients: ['tomato juice', 'lemon juice', 'tobasco', 'worcestershire'],
+        spirit: 'Vodka',
+        altName: '',
+    },
+    {
+        name: 'Corpse Reviver',
+        ingredients: ['triple sec', 'lillet blanc', 'lemon juice', 'absinthe'],
+        spirit: 'Gin',
+        altName: 'Corpse Reviver #2'
+    },
+    {
+        name: 'Daiquiri',
+        ingredients: ['lime juice', 'sugar'],
+        spirit: 'Rum',
+        altName: 'Daquiri',
+    },
+    {
+        name: 'French 75',
+        ingredients: ['champagne', 'sugar', 'lemon juice'], 
+        spirit: 'Gin',
+        altName: '',
+    },
+    {
+        name: 'Gimlet',
+        ingredients: ['lime juice', 'sugar'],
+        spirit: 'Gin',
+        altName: '',
+    },
+    {
+        name: 'Mai Tai',
+        ingredients: ['orgeat syrup', 'triple sec', 'sour mix', 'cherry'],
+        spirit: 'Rum',
+        altName: '',
+    },
+    {
+        name: 'Manhattan',
+        ingredients: ['sweet vermouth', 'bitters', 'cherry', 'orange peel'],
+        spirit: 'Whiskey',
+        altName: '',
+    },
+    {
+        name: 'Margarita',
+        ingredients: ['triple-sec', 'lime juice', 'salt'],
+        spirit: 'Tequila',
+        altName: 'Marg',
+    },
+    {
+        name: 'Martini',
+        ingredients: ['dry vermouth', 'olive'],
+        spirit: 'Gin',
+        altName: 'Gin Martini',
+    },
+    {
+        name: 'Mint Julep',
+        ingredients: ['mint', 'sugar', 'water'],
+        spirit: 'Whiskey',
+        altName: 'Julep',
+    },
+    {
+        name: 'Mojito',
+        ingredients: ['sugar', 'lime juice', 'mint', 'club soda'],
+        spirit: 'Rum',
+        altName: 'Mohito',
+    },
+    {
+        name: 'Moscow Mule',
+        ingredients: ['ginger ale', 'lime juice'],
+        spirit: 'Vodka',
+        altName: 'Mule',
+    },
+    {
+        name: 'Old Fashioned',
+        ingredients: ['bitters', 'sugar', 'orange peel'],
+        spirit: 'Whiskey',
+        altName: 'Ol Fashion',
+    },    
+    {
+        name: 'Negroni',
+        ingredients: ['campari', 'sweet vermouth'],
+        spirit: 'Gin',
+        altName: '',
+    },        
+    {   
+        name: 'Paloma',
+        ingredients: ['grapefruit soda', 'lime juice', 'salt'],
+        spirit: 'Tequila', 
+        altName: '',
+    },
+    {
+        name: 'Ranch Water',
+        ingredients: ['lime juice', 'club soda'],
+        spirit: 'Tequila',
+        altName: '',
+    },
+    {
+        name: 'Rusty Nail',
+        ingredients: ['drambuie', 'lemon peel'],
+        spirit: 'Whiskey',
+        altName: '',
+    },
+    {
+        name: 'Sazerac',
+        ingredients: ['absinthe', 'sugar', 'bitters'],
+        spirit: 'Cognac',
+        altName: '',
+    },
+    {
+        name: 'Sex on the Beach',
+        ingredients: ['peach schnapps', 'cranberry juice', 'orange juice'],
+        spirit: 'Vodka',
+        altName: 'Sex',
+    },
+    {
+        name: 'Sidecar',
+        ingredients: ['triple sec', 'lemon juice'],
+        spirit: 'Cognac',
+        altName: '',
+    },
+    {
+        name: 'Spritz',
+        ingredients: ['campari', 'club soda'],
+        spirit: 'Wine',
+        altName: 'Aperol Spritz'
+    },
+    {
+        name: 'Tequila Sunrise', 
+        ingredients: ['grenadine', 'orange juice'],
+        spirit: 'Tequila',
+        altName: ''
+    },
+    {
+        name: 'Tom Collins',
+        ingredients: ['lemon juice', 'sugar', 'club soda'],
+        spirit: 'Gin',
+        altName: 'Collins',
+    },
+    {
+        name: 'Whiskey Sour',
+        ingredients: ['lemon juice', 'sugar'], 
+        spirit: 'Whiskey',
+        altName: '',
+    },
+
+
+];
